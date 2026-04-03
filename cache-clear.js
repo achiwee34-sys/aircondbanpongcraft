7f08223b7ebc786a06306759dc3558cdbee87256
achiwee34-sys <achiwee34@gmail.com>
2026-04-03 18:39:59 +0700

    Add files via upload

--- cache-clear.js
+++ cache-clear.js
@@ -3,7 +3,11 @@
   try {
     var v = localStorage.getItem('scg_ver');
     if (v !== 'v90') {
-      var keysToKeep = ['scg_fb_cfg','scg_firebase','aircon_session','aircon_dark','aircon_lang','aircon_db'];
+      var ver = localStorage.getItem('airtrack_ver') || '';
+      var keysToKeep = ['scg_fb_cfg','scg_firebase','aircon_session','aircon_dark','aircon_lang',
+                        'airtrack_pwa','airtrack_ver','aircon_sigs',
+                        'aircon_sidebar_collapsed','aircon_pdf_cfg'];
+      if (ver) keysToKeep.push('aircon_preserved_' + ver);
       var saved = {};
       keysToKeep.forEach(function(k){ var val=localStorage.getItem(k); if(val) saved[k]=val; });
       localStorage.clear();
