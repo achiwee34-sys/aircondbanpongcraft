14fc9694e85698ffd052ce53cc3361b7c2e6cb8b
achiwee34-sys <achiwee34@gmail.com>
2026-04-03 18:29:37 +0700

    Add files via upload

--- cache-clear.js
+++ cache-clear.js
@@ -2,13 +2,13 @@
 (function(){
   try {
     var v = localStorage.getItem('scg_ver');
-    if (v !== 'v70') {
+    if (v !== 'v90') {
       var keysToKeep = ['scg_fb_cfg','scg_firebase','aircon_session','aircon_dark','aircon_lang','aircon_db'];
       var saved = {};
       keysToKeep.forEach(function(k){ var val=localStorage.getItem(k); if(val) saved[k]=val; });
       localStorage.clear();
       keysToKeep.forEach(function(k){ if(saved[k]) localStorage.setItem(k,saved[k]); });
-      localStorage.setItem('scg_ver','v70');
+      localStorage.setItem('scg_ver','v90');
     }
   } catch(e){}
 })();
